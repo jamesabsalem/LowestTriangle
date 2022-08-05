a9dece95c8943c6ff2375d3138267caefebd17bf
James Absalem Lintu <jamesabsalem@gmail.com>
2022-08-05 12:12:07 +0600

    Add project files.

---
+++ LowestTriangle/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LowestTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = lowestTriangle(100000, 1);
+            Console.WriteLine(x);
+        }
+        public static decimal lowestTriangle(int trianglebase, int area)
+        {
+            var height = (int) Math.Ceiling((decimal) 2 * area / trianglebase);
+            return height;
+        }
+    }
+}
